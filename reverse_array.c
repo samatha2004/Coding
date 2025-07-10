86efbaee5aa080ef850b3e01822241684743dad1
samatha2004 <144702754+samatha2004@users.noreply.github.com>
2025-07-10 10:39:16 +0530

    Create reverse_array.c

---
+++ reverse_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int rev(int arr[],int n)
+{
+int temp;
+for(int i=0;i<n/2;i++)
+{
+temp=arr[i];
+arr[i]=arr[n-i-1];
+arr[n-i-1]=temp;
+}
+}
+
+int main()
+{
+int n;
+scanf("%d",&n);
+int arr[n];
+for(int i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+rev(arr,n);
+for(int i=0;i<n;i++)
+{
+printf("%d ",arr[i]);
+}
+}
